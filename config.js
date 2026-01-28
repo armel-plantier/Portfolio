a45dc9be5df21bcc1d15f109ab0cf95dd70e4b51
armel-plantier <257267580+armel-plantier@users.noreply.github.com>
2026-01-28 12:48:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const config = {
 
     // --- 2. PROFIL & RÉSEAUX ---
     profile: {
-        // FAVICON : Option 4 (Tes initiales AP en violet sur fond sombre)
+        // FAVICON : Option 4 (Initiales AP en violet)
         favicon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%2220%22 fill=%22%23151925%22/><text x=%2250%22 y=%2265%22 font-family=%22Arial, sans-serif%22 font-weight=%22bold%22 font-size=%2250%22 text-anchor=%22middle%22 fill=%22%236366f1%22>AP</text></svg>",
 
         avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSga_rtaXowL4eH0pqlypM_kgAHCb_gGhUTLA&s",
