f38927e342cb64fb8b0799e4d188cdf80864fcae
armel-plantier <257267580+armel-plantier@users.noreply.github.com>
2026-02-02 16:30:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,6 +10,10 @@ const config = {
 
     // --- 2. PROFIL & RÉSEAUX ---
     profile: {
+        // --- API GITHUB (Pour la date de mise à jour) ---
+        githubUser: "armel-plantier", 
+        githubRepo: "Portfolio", // ⚠️ METTRE LE VRAI NOM DU REPO ICI (ex: "armel-plantier.github.io" ou "v2")
+
         favicon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%2220%22 fill=%22%23151925%22/><text x=%2250%22 y=%2265%22 font-family=%22Arial, sans-serif%22 font-weight=%22bold%22 font-size=%2250%22 text-anchor=%22middle%22 fill=%22%236366f1%22>AP</text></svg>",
         avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSga_rtaXowL4eH0pqlypM_kgAHCb_gGhUTLA&s",
         name: "Armel Plantier",
@@ -17,7 +21,7 @@ const config = {
         bio: "Passionné par l'architecture réseau et le durcissement système. J'aime automatiser avec Bash, configurer des VLANs et analyser des trames Wireshark.",
         status: "Recherche active d'alternance",
         
-        // NOUVEL EMAIL : contact@armel-plantier.com (Encodé Base64)
+        // Email : contact@armel-plantier.com (Encodé Base64)
         emailEncoded: "Y29udGFjdEBhcm1lbC1wbGFudGllci5jb20=",
         
         // CLOUDFLARE TURNSTILE (Captcha)
