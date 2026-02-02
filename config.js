e7cb73800c13817502ad03835d4ac87b76cf94b0
armel-plantier <257267580+armel-plantier@users.noreply.github.com>
2026-02-02 16:40:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const config = {
     profile: {
         // --- API GITHUB (Pour la date de mise à jour) ---
         githubUser: "armel-plantier", 
-        githubRepo: "Portfolio.github.io", // ⚠️ METTRE LE VRAI NOM DU REPO ICI (ex: "armel-plantier.github.io" ou "v2")
+        githubRepo: "Portfolio", // ✅ Nom corrigé ici
 
         favicon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%2220%22 fill=%22%23151925%22/><text x=%2250%22 y=%2265%22 font-family=%22Arial, sans-serif%22 font-weight=%22bold%22 font-size=%2250%22 text-anchor=%22middle%22 fill=%22%236366f1%22>AP</text></svg>",
         avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSga_rtaXowL4eH0pqlypM_kgAHCb_gGhUTLA&s",
