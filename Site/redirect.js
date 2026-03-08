a7b5d6df83a52c69e6d7be9bddc2de0aaa132713
armel-plantier <257267580+armel-plantier@users.noreply.github.com>
2026-03-08 21:36:42 +0100

    Update redirect.js

--- Site/redirect.js
+++ Site/redirect.js
@@ -1,6 +1,11 @@
 (function() {
     var segments = window.location.pathname.split('/').filter(Boolean);
+
     if (segments[0] === 'procedures' && segments[1]) {
         window.location.href = 'https://armel-plantier.com/?proc=' + encodeURIComponent(segments[1]);
+    }
+
+    if (segments[0] === 'projet-technova' && segments[1]) {
+        window.location.href = 'https://armel-plantier.com/?proj=' + encodeURIComponent(segments[1]);
     }
 })();
