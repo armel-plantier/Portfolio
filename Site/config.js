7fcb87b87e8591a43375eedad208256e2132ddd1
armel-plantier <257267580+armel-plantier@users.noreply.github.com>
2026-03-08 20:14:43 +0100

    Update config.js

--- Site/config.js
+++ Site/config.js
@@ -114,6 +114,17 @@ experiences: [
         }
     ],
 
+// --- 7b. PROCÉDURES ---
+    // Pour ajouter une procédure : remplis un bloc et dépose le PDF dans Documents/Procédures/
+    procedures: [
+        {
+            title: "Syslog",
+            description: "Mise en place et configuration de Syslog.",
+            path: "syslog.pdf",
+            tags: ["Administration Système", "Logs"]
+        },
+    ],
+
 // --- 7. CERTIFICATIONS ---
     certifications: [
         { 
