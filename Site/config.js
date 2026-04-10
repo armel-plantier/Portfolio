5e7ac4a5381357b285e28fbf715e77b0e45ff5ee
armel-plantier <257267580+armel-plantier@users.noreply.github.com>
2026-04-11 00:10:39 +0200

    Update config.js

--- Site/config.js
+++ Site/config.js
@@ -366,8 +366,8 @@ experiences: [
     documentsE5: [
         {
             title: "Tableau de synthèse E5",
-            description: "Tableau de synthèse E5 — BTS SIO Session 2026",
-            path: "E5/03-_tableau-de-synthese-E5.xlsx",
+            description: "Tableau de synthèse des réalisations professionnelles — BTS SIO Session 2026",
+            path: "E5/e5-tableau.xlsx",
             icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>`,
             type: "xlsx"
         }
