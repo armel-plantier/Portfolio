63b2fe23a96653eee8bad4b42c4ab00e4ef71033
armel-plantier <257267580+armel-plantier@users.noreply.github.com>
2026-04-28 17:09:03 +0200

    Update config.js

--- Site/config.js
+++ Site/config.js
@@ -384,6 +384,20 @@ experiences: [
             path: "E5/e5-tableau.xlsx",
             icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>`,
             type: "xlsx"
+        },
+        {
+            title: "Attestation de stage — Ellix Informatique",
+            description: "Stage de 1 mois — Mai à Juin 2025 — Hérouville-Saint-Clair",
+            path: "E5/Attestation_de_stage_Ellix_Informatique.pdf",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>`,
+            type: "pdf"
+        },
+        {
+            title: "Attestation de stage — Fondation de la Miséricorde",
+            description: "Stage de 7 semaines — Décembre 2025 à Février 2026 — Caen",
+            path: "E5/Attestation_de_stage_Misericorde.pdf",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>`,
+            type: "pdf"
         }
     ]
     
